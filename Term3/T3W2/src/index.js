5a8afc45f7441c81468578fedd2f3c8e7ab65ce9
Nhi Tran <98810653+nhithutran@users.noreply.github.com>
2022-07-25 21:38:35 +1000

    add jest

---
+++ Term3/T3W2/src/index.js
@@ -0,0 +1,34 @@
+function sum(a,b) {
+    if (a < 0) {
+        console.log("The sum includes a negative number!");
+    }
+    return a + b;
+}
+
+// Common JS Moduel syntax
+module.exports = {
+    sum
+};
+
+function tempFunc1() {
+
+}
+
+function tempFunc2() {
+    
+}
+
+function tempFunc3() {
+    
+}
+
+function tempFunc4() {
+    
+}
+
+function tempFunc5() {
+    
+}
+
+// ES6 syntax (Teh Es Module format)
+// export default sum;
